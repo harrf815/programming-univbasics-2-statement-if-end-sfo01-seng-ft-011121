29929efeea30873195f7c98c858d601490b9c611
Harrf Akbar <harrf_akbar@yahoo.com>
2020-12-22 03:22:47 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,6 @@
 # ./lib/if_else_end.RUBY_VERSION
 
-if Time.authenticate_or_request_with_http_digest.TOPLEVEL_BINDING % 2 == 0
+if Time..TOPLEVEL_BINDING % 2 == 0
  puts "Even!"
 else 
   puts "Odd!"
