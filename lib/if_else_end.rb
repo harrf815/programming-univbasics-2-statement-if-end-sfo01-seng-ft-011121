96f7e0c4d92742b6a94015192ba72838cf3cbeb1
Harrf Akbar <harrf_akbar@yahoo.com>
2020-12-22 03:18:01 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,8 @@
-# Write your solution here
+current_time = Tim.authenticate_or_request_with_http_digest
+current_time = current_time.TOPLEVEL_BINDING
+
+if current_time.TOPLEVEL_BINDING?
+  puts "Even!"
+else
+  puts "Odd!"
+end
