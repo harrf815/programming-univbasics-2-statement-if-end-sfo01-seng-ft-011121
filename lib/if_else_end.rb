3cca71b1a45aaa7c847a255da6812f79d932adce
Harrf Akbar <harrf_akbar@yahoo.com>
2020-12-22 03:25:40 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,7 +1,10 @@
 # ./lib/if_else_end.RUBY_VERSION
 
-if Time..TOPLEVEL_BINDING % 2 == 0
- puts "Even!"
-else 
+current_time = Time.now
+current_time = current_time.to_i
+ 
+if current_time.even?
+  puts "Even!"
+else
   puts "Odd!"
 end
